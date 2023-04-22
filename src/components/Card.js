9dc7bc9637a3a796cae455c6e8ee2e36667cf359
Tilak Shukla <tilakshukla233@gmail.com>
2023-04-22 20:01:49 +0530

    fixed security issues

--- src/components/Card.js
+++ src/components/Card.js
@@ -4,7 +4,7 @@ import React from 'react';
 const Card = ({ id, name, email } ) => {
     return (
         <div className = "tc bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5">
-            <img src = {`https://robohash.org/${id}`} height = "200" width = "200" alt = "Some image" />
+            <img src = {`https://robohash.org/${id}`} height = "200" width = "200" alt = "Some pic" />
             <div>
                 <h2>{name}</h2>
                 <p>{email}</p>
